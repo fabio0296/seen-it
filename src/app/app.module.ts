8955a91da997e0681ee9ba3bf82f40a7df5f08af
Fabio Carballo <fabio.carballo96@gmail.com>
2017-06-18 14:26:13 -0600

    material design integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material'
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
